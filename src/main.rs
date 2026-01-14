b371837ac809d112d0f50e1505adc9e86c18cb6e
Manan Gupta <guptamanan1991@gmail.com>
2026-01-15 01:35:57 +0530

    Add macro (eval_f64!) and other steps for numerical evaluation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+#![allow(non_snake_case)]
+use ahash::HashMap;
+
 use stupid_simple_dotenv::to_env;
 use symbolica::{atom::Atom, atom::AtomCore, parse, printer::PrintOptions, symbol, LicenseManager};
+
+macro_rules! eval_f64 {
+    ($atom:ident, $const_map:ident) => {
+        $atom
+            .evaluate(|r| r.to_f64(), &$const_map, &HashMap::default())
+            .unwrap()
+    };
+}
 
 fn main() {
     to_env().ok();
@@ -7,6 +18,12 @@ fn main() {
 
     // Symbols
     let (Q1, Q3, Z1, Z2, Z3) = symbol!("Q1", "Q3", "Z1", "Z2", "Z3");
+    let mut values = HashMap::default();
+    values.insert(Atom::var(Z1), 2.);
+    values.insert(Atom::var(Z2), 4.);
+    values.insert(Atom::var(Z3), 6.);
+    values.insert(Atom::var(Q1), 0.11);
+    values.insert(Atom::var(Q3), 0.445);
     let Q2 = (Q1 + Q3) * (-1) + 1;
     // println!("{}", &Q2.printer(PrintOptions::latex()));
 
@@ -16,16 +33,28 @@ fn main() {
 
     // Mean phenotype
     let EZ = Atom::var(Q1) * Atom::var(Z1) + &Q2 * Atom::var(Z2) + Atom::var(Q3) * Atom::var(Z3);
+    println!("EZ = {}", eval_f64!(EZ, values));
 
     // Mean allele dosage (a1 = 2, a2 = 1, a3 = 0)
     let Ea = Atom::var(Q1) * 2 + &Q2;
     let Eaa = Atom::var(Q1) * 4 + &Q2;
     let EaZ = Atom::var(Q1) * Atom::var(Z1) * 2 + Atom::var(Z2) * &Q2;
     // println!("EaZ = {}", &EaZ.printer(PrintOptions::latex()));
+    println!(
+        "Ea = {}\nEaa = {}\nEaZ = {}",
+        eval_f64!(Ea, values),
+        eval_f64!(Eaa, values),
+        eval_f64!(EaZ, values)
+    );
 
     // Z vs a regression
     let Beta_Za = ((&EaZ - &Ea * &EZ) / (&Eaa - &Ea * &Ea)).expand();
     let K_Za = (&EZ - &Beta_Za * &Ea).expand();
+    println!(
+        "Beta_Za = {}\nK_Za = {}",
+        eval_f64!(Beta_Za, values),
+        eval_f64!(K_Za, values)
+    );
     // println!(
     //     "Beta_Za = {}\nK_Za = {}",
     //     &Beta_Za.printer(PrintOptions::sympy()),
@@ -47,6 +76,7 @@ fn main() {
     let EWA = Atom::var(Q1) * Atom::var(Z1) * &A1
         + &Q2 * Atom::var(Z2) * &A2
         + Atom::var(Q3) * Atom::var(Z3) * &A3;
+    println!("EWA = {}", eval_f64!(EWA, values));
 
     // Post selection frequency
     let Q11 = Atom::var(Q1) * (Atom::var(Z1) / &EZ);
@@ -55,6 +85,7 @@ fn main() {
 
     // Fisherian selection response (Cov(g,w)/EW)
     let SR_F = (&EWA - &EZ * &EA) / &EZ;
+    println!("SR_F = {}", eval_f64!(SR_F, values));
     // println!("SR_F = {}", &SR_F.together().printer(PrintOptions::latex()));
 
     // Mating
@@ -76,17 +107,25 @@ fn main() {
     let Q111 = &f11 + &f12 / 2 + &f21 / 2 + &f22 / 4;
     let Q211 = (&f12 + &f21 + &f22 + &f23 + &f32) / 2 + &f13 + &f31;
     let Q311 = &f22 / 4 + (&f23 + &f32) / 2 + &f33;
+    println!(
+        "Q111 = {}\nQ211 = {}\nQ311 = {}",
+        eval_f64!(Q111, values),
+        eval_f64!(Q211, values),
+        eval_f64!(Q311, values)
+    );
 
     // Generation 2 phenotypic mean
     let Ez = Atom::var(Z1) * &Q111 + Atom::var(Z2) * &Q211 + Atom::var(Z3) * &Q311;
 
     // Total selection response
     let SR_T = &Ez - &EZ;
+    println!("SR_T = {}", eval_f64!(SR_T, values));
 
     // Discrepancy: TSR - FSR
     let Ds_1 = &SR_T - &SR_F;
     // Full Ds expression
     let Ds = Ds_1.expand();
+    println!("Ds = {}", eval_f64!(Ds, values));
     println!(
         "Raw Ds expression:\n{}",
         &Ds.printer(PrintOptions::sympy().hide_namespace("algebra_of_selection"))
@@ -94,6 +133,7 @@ fn main() {
 
     // Full factorization of Ds
     let Ds_factored = &Ds.factor();
+    println!("Ds_factored = {}", eval_f64!(Ds_factored, values));
     println!(
         "Full factorized Ds:\n{}",
         &Ds_factored.printer(PrintOptions::sympy().hide_namespace("algebra_of_selection"))
